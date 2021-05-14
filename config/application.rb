82ad98348bf0db0910ab2cc489f8f58a1d2e8882
soramar <fujitanisora@i.softbank.jp>
2021-05-14 18:27:19 +0900

    指定箇所の修正

--- config/application.rb
+++ config/application.rb
@@ -23,17 +23,16 @@ module RunteqNormal
     config.generators do |g|
       g.assets false
       g.helper false
-      g.test_framework false     
+      g.test_framework false
     end
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
   end
 end
-    # Initialize configuration defaults for originally generated Rails version.
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-    # Don't generate system test files.
-  
+# Initialize configuration defaults for originally generated Rails version.
+# Settings in config/environments/* take precedence over those specified here.
+# Application configuration can go into files in config/initializers
+# -- all .rb files in that directory are automatically loaded after loading
+# the framework and any gems in your application.
+# Don't generate system test files.

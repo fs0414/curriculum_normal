f9c0d584e02a2c6e28a964217576e75257ef8841
soramar <fujitanisora@i.softbank.jp>
2021-05-14 10:11:23 +0900

    指定箇所の修正を行いました

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,7 +3,7 @@ class UserSessionsController < ApplicationController
 
   def create
     @user = login(params[:email], params[:password])
-    if @user.save
+    if @user
       redirect_back_or_to root_path
     else
       render :new

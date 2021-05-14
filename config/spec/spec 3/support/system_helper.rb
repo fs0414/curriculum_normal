ac25bd172bc5de10162d8289d995971547a817fb
soramar <fujitanisora@i.softbank.jp>
2021-05-14 10:34:12 +0900

    指定箇所の修正を行いました

--- config/spec/spec 3/support/system_helper.rb
+++
@@ -1,9 +0,0 @@
-module SystemHelper
-  def login_as_general
-    general_user = create(:user)
-    visit '/login'
-    fill_in 'Email', with: general_user.email
-    fill_in 'Password', with: '12345678'
-    click_button 'ログイン'
-  end
-end

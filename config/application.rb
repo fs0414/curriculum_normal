4df4c17e24edfd1374aabc9e0ee22281a04584fc
soramar <fujitanisora@i.softbank.jp>
2021-05-14 19:04:01 +0900

    指定箇所の修正

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module RunteqNormal
     end
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
 # Initialize configuration defaults for originally generated Rails version.

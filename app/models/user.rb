80f407d5d807fc84fe322dbce473fed47852300a
soramar <fujitanisora@i.softbank.jp>
2021-05-28 23:59:00 +0900

    指定箇所の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :email, presence: true, uniqueness: true 
+  validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
 

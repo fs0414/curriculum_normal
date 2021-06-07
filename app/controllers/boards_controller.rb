d04726c64075bb73b8861581ca807b1b59bebd73
soramar <fujitanisora@i.softbank.jp>
2021-06-08 00:04:53 +0900

    ブックマーク機能の実装を行いました

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -43,6 +43,10 @@ class BoardsController < ApplicationController
     redirect_to boards_path, success: t('.success')
   end
 
+  def bookmarks
+    @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc)
+  end
+
   private
 
   def board_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create] 
   resources :boards do
-    resources :comments, only: %i[create], shallow: true
+    resources :comments, only: %i[create],  shallow: true
+    collection do
+      get :bookmarks
+    end
   end
+  resources :bookmarks, only: %i[create destroy]
 end

6f56d181a41d783c68c5202b74ee05e0ed300936
soramar <fujitanisora@i.softbank.jp>
2021-06-17 07:11:32 +0900

    プロフィール編集機能の実装を行いました

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,23 @@
+class ProfilesController < ApplicationController
+  def show; end
+  
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to profiles_path, success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: User.model_name.human)
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :avatar)
+  end
+end
